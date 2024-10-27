2acc01847be721a10df30e4856406ce7ed4c368d
sudharsan410 <sudharsanp38@gmail.com>
2024-10-27 15:46:38 +0530

    Some changes in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
                 href="#"
                 className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
               >
-                Create an account
+                Create an Account
               </a>
             </div>
           </div>
